603fb2c387dfdd584f503fda5da821bab21861c6
Varsha Shiburaj <varshashiburaj@gmail.com>
2019-12-22 15:12:41 +0530

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,19 @@
+#include "ACS712.h"
+char watt[5];
+ACS712 sensor(ACS712_30A, A0);
+void setup() {
+  Serial.begin(115200);
+  sensor.calibrate();
+}
+void loop() {
+  float V= 230;
+  float I = sensor.getCurrentAC();
+  float P = V * I;
+  last_time = current_time;
+  current_time = millis();    
+  Wh = Wh+  P *(( current_time -last_time) /3600000.0) ; 
+   dtostrf(Wh, 4, 2, watt);
+dtostrf(floatvar, StringLengthIncDecimalPoint, numVarsAfterDecimal, charbuf);
+ Serial.write(watt);
+  delay(10000);
+}
